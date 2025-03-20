eb39b20aaf32f98a7822c94fb56d2f47572eedd0
webdevhacker <i.kumar.sharan@gmail.com>
2025-03-20 14:18:44 +0530

    Changes to the user email templates accnt verification

--- api/config/emailTemplates.js
+++ api/config/emailTemplates.js
@@ -458,7 +458,7 @@ export const EMAIL_VERIFY_TEMPLATE = `
                     <td class="content-cell">
                       <div class="f-fallback">
                         <h1>Dear {{name}},</h1>
-                        <p>A request is received to to verify your account for the email id {{email}}. Kindly enter the below OTP to verify your account.</p>
+                        <p>A request is received to verify your account associated with the email id {{email}}. Kindly enter the below OTP to verify your account.</p>
                         <!-- Action -->
                         <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
                           <tr>
