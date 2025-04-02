ac4b784c940a0f62df724b54e1ff006f19dc2827
webdevhacker <i.kumar.sharan@gmail.com>
2025-04-02 11:17:28 +0530

    changes to the cors headers in api/versel.json file

--- client/src/pages/Blog/AddBlog.jsx
+++ client/src/pages/Blog/AddBlog.jsx
@@ -83,8 +83,8 @@ const AddBlog = () => {
 
             const response = await fetch(`${getEnv('VITE_API_BASE_URL')}/blog/add`, {
                 method: 'POST',
-                mode:'no-cors',
-                //allowedHeaders: { 'Content-type': 'application/json' },
+                //mode: 'no-cors',
+                headers: { 'Content-type': 'application/json' },
                 credentials: 'include',
                 body: formData
             })
